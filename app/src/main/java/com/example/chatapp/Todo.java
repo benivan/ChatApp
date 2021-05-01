6d19e1d31cc55754bb02d68258b85a0b3f0e4512
Vikash Kumar <itsmevikash09@gmail.com>
2021-05-01 17:09:50 +0530

    todo activity_main.xml and class created

---
+++ app/src/main/java/com/example/chatapp/Todo.java
@@ -0,0 +1,27 @@
+package com.example.chatapp;
+
+public class Todo {
+    private String title;
+    private  Boolean isChecked = false;
+
+    public Todo(String title, Boolean isChecked) {
+        this.title = title;
+        this.isChecked = isChecked;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(Boolean checked) {
+        isChecked = checked;
+    }
+}
